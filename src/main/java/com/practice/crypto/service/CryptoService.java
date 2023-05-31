53b1cfb2acaa59aaf57d88d6a353b4b2e213d5fe
Gokulan Viswanathan <25838622+gokulanviswanathan@users.noreply.github.com>
2023-05-31 19:50:44 +0530

    Encryption-Decryption via AES-ECB mode

---
+++ src/main/java/com/practice/crypto/service/CryptoService.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+package com.practice.crypto.service;
+
+public interface CryptoService {
+
+	public String getCryptoData(String cryptoType, String cryptoInput, String trackingId);
+
+}
